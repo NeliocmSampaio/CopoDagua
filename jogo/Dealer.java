9eca453bd4b69c4ec311f7b4b2041851ac23cca9
Dtrabs <Dtrabs@users.noreply.github.com>
2017-05-10 00:49:18 -0300

    pronto

--- jogo/Dealer.java
+++ jogo/Dealer.java
@@ -1,14 +1,33 @@
+/**
+ * 
+ */
 package jogo;
 
+import baralho.Carta;
 import baralho.Baralho;
 
+/**
+ * @author MARCIO
+ *
+ */
 public class Dealer {
-	static Baralho baralho;
-	
-	public static void insereAsDeEspadas()
-	{
+
+	public void embaralhaBaralho (Baralho baralho) {
 		
+		baralho.embaralhar();
 	}
 	
-	
+	public void insereAsDeEspadas (Baralho baralho) {
+		
+		Carta aux = new Carta();
+		aux.setNype(1);
+		aux.setValor(1);
+		
+		baralho.adicionarCartaEmCima(aux);
+	}
+
+	public Carta darUmaCarta (Baralho baralho) {
+		
+		return baralho.removerCartaDeCima();
+	}
 }
